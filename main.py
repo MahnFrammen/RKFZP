c3238f51f6168ab83cfb02e5fdb1554bee6afd10
MahnFrammen <71048879+MahnFrammen@users.noreply.github.com>
2021-10-03 23:02:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ with warnings.catch_warnings():
 h=0.3 #Given values for ODE
 x0=2
 y0=1
-xpoints=[x0]
-ypoints=[y0]
+xpoints=[x0] #array for storing X values
+ypoints=[y0] #Array for storing Y values 
 
 #State equations and define functions
 def dy_dx(y,x):
-    return -y + np.log(x)
+    return -y + np.log(x) #Provided Equation
 def RungeKuttaFehlberg(x,y):
     return -y + np.log(x)
-#Calculates k1-k4, x and y values
+#Calculates k1-k4, x and y solutions
 def RKFAlg(x0,y0,h):
     k1 = RungeKuttaFehlberg(x0,y0)
     k2 = RungeKuttaFehlberg(x0+(h/2),y0+((h/2)*k1))
@@ -36,19 +36,19 @@ def RKFAlg(x0,y0,h):
     x1 = x0+h
     x1 = round(x1,2)
     print("Point (x(n+1),y(n+1)) =",(x1,y1))
-    return((x1,y1))
+    return((x1,y1)) #Returns as ordered pair
 
 #Define range for number of calculations
 for i in range(2000):
     print(f"Y{i+1}".format(i)) #Solution value format
     x0,y0 = RKFAlg(x0,y0,h) #Calls RKF Function
-    xpoints.append(x0)
+    xpoints.append(x0) #Saves values into array
     ypoints.append(y0)
     y0 = 1
     ODEy1 = odeint(dy_dx,y0,xpoints)
 
 #Runge-Kutta Graph
-plt.plot(xpoints,ypoints,'b:',linewidth = 1)
+plt.plot(xpoints,ypoints,'b:',linewidth = 1) #command to plot lines using various colors and widths
 plt.suptitle("RKF Graph")
 plt.xlabel("x Points")
 plt.ylabel("y Points")
@@ -71,7 +71,7 @@ plt.ylabel("Y")
 plt.show()
 
 #Function for plotting the difference graph
-diff = []
+diff = [] #array to store difference
 for i in range(len(xpoints)):
     diff.append(ypoints[i]-ODEy1[i])
 plt.plot(xpoints,diff)
